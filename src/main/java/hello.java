a943712519f8b642cfdf6edf7d8fd762e2d74f86
FangHou <hf5902383@gmail.com>
2024-02-21 11:38:41 +0800

    提示消息 - main 第三次提交

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("七月七日长生殿");
         System.out.println("夜半无人私语时");
+        System.out.println("在天愿作比翼鸟");
     }
 }
